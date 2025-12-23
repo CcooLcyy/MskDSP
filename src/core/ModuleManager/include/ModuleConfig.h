953d5ce2eb4a92efa7719dec91ab23e15716253d
Dannys <mutex_@outlook.com>
2025-12-24 01:04:42 +0800

    [feature] 1. 暂时移除toml 2. 更新vcpkg基线 3. 使用json作为配置文件格式 4. 使用现代方式管理protobuf和grpc 5. 增加ModuleConfig类

--- src/core/ModuleManager/include/ModuleConfig.h
+++ src/core/ModuleManager/include/ModuleConfig.h
@@ -1,10 +1,20 @@
 #pragma once
 
-#include <toml++/toml.h>
+#include <google/protobuf/message.h>
+
+#include <boost/json.hpp>
+
+#include "ModuleConfig.pb.h"
 
 class ModuleConfig {
 public:
   ModuleConfig();
   ~ModuleConfig();
-  
+
+  ModuleConfigProto::Config &getConfig();
+
+private:
+  const std::string defaultConfigPath_{"./conf/moduleConfig.json"};
+  void loadConfig();
+  ModuleConfigProto::Config configMessage_;
 };
